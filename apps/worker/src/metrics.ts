383f0f53b493192ef368e2e8725822ad67e02284
Collins1M <collomwas10@gmail.com>
2026-09-06 19:18:42 +0300

    chore: update package dependencies and add obug module
    - Added obug module version 1.0.0 to devDependencies.
    - Replaced prom-client with @prometheus-io/client in multiple package.json files.
    - Updated glob dependency version in package.json.

--- apps/worker/src/metrics.ts
+++ apps/worker/src/metrics.ts
@@ -1,5 +1,5 @@
 import { createServer } from "node:http";
-import * as client from "prom-client";
+import * as client from "@prometheus-io/client";
 import { logger } from "./logger";
 
 export const register = new client.Registry();
